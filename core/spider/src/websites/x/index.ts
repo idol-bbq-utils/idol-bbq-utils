983d4d06f0b28644f886f84ba2ffe648a9293fd1
ChocoLZS <chocoielzs@gmail.com>
2024-07-08 14:00:36 +0800

    feat: grab tweet

--- core/spider/src/websites/x/index.ts
+++ core/spider/src/websites/x/index.ts
@@ -1,16 +1,38 @@
-import { Page } from 'puppeteer'
+import { ElementHandle, Page } from 'puppeteer'
 import { TweetTabs } from './types'
+
+async function tweetArticleParser(tweet: ElementHandle<HTMLElement>) {
+  const html = await tweet.evaluate((e) => e.innerHTML)
+  return html
+}
 
 export async function grabTweets(page: Page, url: string) {
   // Navigate the page to a URL
-  await page.goto('https://x.com/hanamiya_nina/')
+  await page.goto(url)
 
   // Set screen size
   await page.setViewport({ width: 1080, height: 1024 })
+  // Click on the tweets tab
   const tablist = await page.$('div[role="tablist"]')
   const tabs = await tablist?.$$('div[role="presentation"]')
   const tab = await tabs?.[TweetTabs.TWEETS].$('a')
   await tab?.click()
+
+  const article_wrapper = await page.waitForSelector('nav[role="navigation"] + section > div')
+  // wait for article to load
+  await article_wrapper?.waitForSelector('article')
+  const raw_articles = await article_wrapper?.$$('article')
+  //   const articles = await Promise.all(raw_articles?.map(tweetArticleParser) ?? [])
+  const articles = raw_articles && (await tweetArticleParser(raw_articles?.[0]))
+  console.log(articles)
+}
+
+export async function grabFans(page: Page, url: string) {
+  // Navigate the page to a URL
+  await page.goto(url)
+
+  // Set screen size
+  await page.setViewport({ width: 1080, height: 1024 })
   const profile_content = await page.$('section[aria-labelledby="accessible-list-0"]')
   const html = await profile_content?.evaluate((e) => e.innerHTML)
   console.log(html)
