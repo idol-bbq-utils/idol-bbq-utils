d997dffe63114cd99be9d37ec64dd8008a96a90c
ChocoLZS <chocoielzs@gmail.com>
2024-07-08 10:38:51 +0800

    feat: get tweets

--- core/spider/src/websites/x/index.ts
+++ core/spider/src/websites/x/index.ts
@@ -1,15 +1,17 @@
 import { Page } from 'puppeteer'
+import { TweetTabs } from './types'
 
 export async function grabTweets(page: Page, url: string) {
   // Navigate the page to a URL
-  await page.goto(url)
+  await page.goto('https://x.com/hanamiya_nina/')
 
   // Set screen size
   await page.setViewport({ width: 1080, height: 1024 })
   const tablist = await page.$('div[role="tablist"]')
-  const tabs = await tablist?.$$('div[role="presentation]')
-  tabs?.forEach(async (tab) => {
-    const content = await tab.evaluate((e) => e.innerHTML)
-    console.log(content)
-  })
+  const tabs = await tablist?.$$('div[role="presentation"]')
+  const tab = await tabs?.[TweetTabs.TWEETS].$('a')
+  await tab?.click()
+  const profile_content = await page.$('section[aria-labelledby="accessible-list-0"]')
+  const html = await profile_content?.evaluate((e) => e.innerHTML)
+  console.log(html)
 }
--- core/spider/src/websites/x/types.ts
+++ core/spider/src/websites/x/types.ts
@@ -3,3 +3,5 @@ enum TweetTabs {
   REPLIES = 1,
   MEDIA = 2,
 }
+
+export { TweetTabs }
