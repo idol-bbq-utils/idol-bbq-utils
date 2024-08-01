66af4b9cb3f4a63e04829f33b28c48b2511cadca
ChocoLZS <chocoielzs@gmail.com>
2024-08-01 20:59:42 +0800

    fix: gallery-dl

--- core/tweet-forwarder/src/middleware/collector/x.ts
+++ core/tweet-forwarder/src/middleware/collector/x.ts
@@ -279,7 +279,7 @@ class XCollector extends Collector {
                 let images = [] as string[]
                 if (config?.media) {
                     for (const article of articles) {
-                        if (article.has_media) {
+                        if (article.has_media && article.tweet_link) {
                             images = images.concat(
                                 downloadMediaFiles(`https://x.com${article.tweet_link}`, config.media.gallery_dl),
                             )
