8b5937f401abf6494d1322bbfe9c888e0f3ba7de
chocoie <chocoielzs@gmail.com>
2024-10-21 14:23:10 +0800

    chore: build preparation

--- app/tweet-forwarder/src/db/client.ts
+++ app/tweet-forwarder/src/db/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from '../../prisma/generated/client/index.js'
+import { PrismaClient, Prisma } from '../../prisma/client/index.js'
 
 const prisma = new PrismaClient()
 
