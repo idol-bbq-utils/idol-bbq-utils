7a1aa4d64cf13d9822fca3cba2f1aba315eb1b48
ChocoLZS <chocoielzs@gmail.com>
2025-06-14 20:26:41 +0800

    add render type

--- app/tweet-forwarder/src/managers/forwarder-manager.ts
+++ app/tweet-forwarder/src/managers/forwarder-manager.ts
@@ -420,7 +420,7 @@ class ForwarderPools extends BaseCompatibleModel {
                 path: string
                 media_type: MediaType
             }>
-            if (cfg_forwarder?.render_type === 'img') {
+            if (cfg_forwarder?.render_type?.startsWith('img')) {
                 try {
                     const imgBuffer = await this.ArticleConverter.articleToImg(
                         cloneDeep(article),
@@ -514,7 +514,9 @@ class ForwarderPools extends BaseCompatibleModel {
             }
             const fullText = articleToText(article)
             // 获取需要转发的文本，但如果已经执行了文本转图片，则只需要metaline
-            const text = articleToImgSuccess ? formatMetaline(article) : fullText
+            let text = articleToImgSuccess ? formatMetaline(article) : fullText
+            // 如果render_type是img，则不需要文本
+            text = cfg_forwarder?.render_type === 'img' ? '' : text
 
             let error_for_all = true
 
