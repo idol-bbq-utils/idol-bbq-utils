481e93442981120b4e07b4d7c249037203fb65d0
ChocoLZS <chocoielzs@gmail.com>
2025-09-12 16:20:49 +0800

    feat: new list api

--- core/spider/src/spiders/x.ts
+++ core/spider/src/spiders/x.ts
@@ -19,7 +19,7 @@ import { ClientTransaction, handleXMigration } from 'x-client-transaction-id'
 import { v4 as uuidv4 } from 'uuid'
 import { noop } from 'puppeteer-core/lib/esm/third_party/rxjs/rxjs.js'
 
-type XListApiEngine = 'api-statuses' | 'api-member'
+type XListApiEngine = 'api-statuses' | 'api-member' | 'api-graphql'
 
 enum ArticleTypeEnum {
     /**
@@ -41,6 +41,7 @@ enum XApis {
     UserTweets = 'UserTweets',
     UserTweetsAndReplies = 'UserTweetsAndReplies',
     UserByScreenName = 'UserByScreenName',
+    // ListLatestTweetsTimeline = 'ListLatestTweetsTimeline',
 }
 
 const apis = Object.values(XApis)
@@ -151,6 +152,8 @@ class XListSpider extends BaseSpider {
     PUBLIC_TOKEN =
         'Bearer AAAAAAAAAAAAAAAAAAAAAFQODgEAAAAAVHTp76lzh3rFzcHbmHVvQxYYpTw%3DckAlMINMjmCwxUcaXbAN4XqJVdgMJaHqNOFgPMK0zN1qLqLQCF'
 
+    GRAPHQL_API_CLIENT: XApiClient = new XApiClient()
+
     async _crawl<T extends TaskType>(
         url: string,
         page: Page,
@@ -175,9 +178,11 @@ class XListSpider extends BaseSpider {
             this.log?.warn('Replies are not supported in this mode for now.')
             this.log?.info(`Trying to grab tweets for ${id}.`)
             let res = [] as Array<GenericArticle<Platform.X>>
+            const cookie_string = (await page.browserContext().cookies()).map((c) => `${c.name}=${c.value}`).join('; ')
             if (config.crawl_engine === 'api-statuses') {
-                const cookie_string = (await page.browserContext().cookies()).map((c) => `${c.name}=${c.value}`).join('; ')
                 res = await this.grabTweets(id, cookie_string)
+            } else if (config.crawl_engine === 'api-graphql') {
+                res = await this.GRAPHQL_API_CLIENT.grabTweetsFromList(id, cookie_string)
             } else {
                 res = await this.grabTweetsPoor(id)
             }
@@ -355,7 +360,7 @@ class XApiClient {
                 }
             }
 
-            this.cache.set(CACHE_KEYS.TransactionClient, transaction, 60 * 60 * 6) // 6h
+            this.cache.set(CACHE_KEYS.TransactionClient, transaction, 60 * 60 * 24) // 24h
         }
 
         return transaction
@@ -604,6 +609,38 @@ class XApiClient {
             throw new Error(`Failed to fetch user info for ${id}`)
         }
         return XApiJsonParser.tweetsFollowsParser(user_info)
+    }
+
+    async grabTweetsFromList(list_id: string, cookie: string) {
+        // const transaction = await this.getTransaction()
+        const query_id = "AbHmcvLWYVxADvWF6zvFWQ" // abs.twimg.com\responsive-web\client-web\sharedloader.Dockbundle.Articlesbundle.AudioSpaceDetailbundle.AudioSpaceDiscoverybundle.AudioSpacebarScr.e8195e0a.js
+        const query_path = `${this.API_PREFIX}/${query_id}/ListLatestTweetsTimeline`
+        // const transaction_id = await transaction.generateTransactionId('GET', query_path)
+        const csrf_token = this.getCsrfToken(cookie)
+        const variables = { listId: list_id, count: 20 }
+        const features = {"rweb_video_screen_enabled":false,"payments_enabled":false,"profile_label_improvements_pcf_label_in_post_enabled":true,"rweb_tipjar_consumption_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"premium_content_api_read_enabled":false,"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"responsive_web_grok_analyze_button_fetch_trends_enabled":false,"responsive_web_grok_analyze_post_followups_enabled":true,"responsive_web_jetfuel_frame":true,"responsive_web_grok_share_attachment_enabled":true,"articles_preview_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"responsive_web_grok_show_grok_translated_post":false,"responsive_web_grok_analysis_button_from_backend":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_grok_image_annotation_enabled":true,"responsive_web_grok_imagine_annotation_enabled":true,"responsive_web_grok_community_note_auto_translation_is_enabled":false,"responsive_web_enhance_cards_enabled":false}
+        const query = this.generateParams(features, variables)
+
+        const url = `${this.BASE_URL}${query_path}?${query.toString()}`
+        const res = await fetch(url, {
+            headers: {
+                ...this.BASE_HEADER,
+                cookie: cookie,
+                // 'x-client-transaction-id': transaction_id,
+                // 'x-client-uuid': uuid,
+                'x-csrf-token': csrf_token || '',
+                // 'x-twitter-active-user': 'yes',
+                // 'x-twitter-auth-type': 'OAuth2Session',
+            },
+        })
+        if (!res.ok) {
+            throw new Error(`Failed to fetch tweets: ${res.statusText}`)
+        }
+        const json = await res.json()
+        if (json.errors) {
+            throw new Error(`Failed to fetch tweets: ${json.errors[0].message}`)
+        }
+        return XApiJsonParser.tweetsArticleParser(json)
     }
 }
 namespace XApiJsonParser {
