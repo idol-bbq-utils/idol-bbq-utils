09d549d68b027bdff57ab090f61e6963db10d5fc
ChocoLZS <chocoielzs@outlook.com>
2026-01-27 16:47:33 +0800

    rm: single app

--- app/tweet-forwarder/src/middleware/forwarder/pipeline/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './types'
-export * from './pipeline'
-export * from './middlewares'
-export * from './renderers'
--- app/tweet-forwarder/src/utils/typeguards.ts
+++
@@ -1,5 +0,0 @@
-function isStringArrayArray(arr: string[] | string[][]): arr is Array<[string, string]> {
-    return arr.length > 0 && Array.isArray(arr[0])
-}
-
-export { isStringArrayArray }
